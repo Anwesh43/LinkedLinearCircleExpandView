c4f10cd95bd55943cf05aa43a4709a2b742260a3
Anwesh43 <anweshthecool0@gmail.com>
2019-05-03 11:44:22 +0530

    adding LinearCircleExpand class

--- linearcircleexpandview/src/main/java/com/anwesh/uiprojects/linearcircleexpandview/LinearCircleExpandView.kt
+++ linearcircleexpandview/src/main/java/com/anwesh/uiprojects/linearcircleexpandview/LinearCircleExpandView.kt
@@ -171,7 +171,31 @@ class LinearCircleExpandView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class LinearCircleExpand(var i : Int) {
+
+        private val root : LCENode = LCENode(0)
+        private var curr : LCENode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
