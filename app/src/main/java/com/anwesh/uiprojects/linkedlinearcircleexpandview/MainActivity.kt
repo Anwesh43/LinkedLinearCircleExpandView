f4a402dc3fb763e6aa644ca9f4f1d4d8a2f597d4
Anwesh43 <anweshthecool0@gmail.com>
2019-05-03 12:05:41 +0530

    adding LinearCircleExpandView class

--- app/src/main/java/com/anwesh/uiprojects/linkedlinearcircleexpandview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlinearcircleexpandview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedlinearcircleexpandview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linearcircleexpandview.LinearCircleExpandView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinearCircleExpandView.create(this)
     }
 }
