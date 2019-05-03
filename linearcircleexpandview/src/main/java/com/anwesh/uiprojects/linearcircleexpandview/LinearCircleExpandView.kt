26ba7b67e1920061ff41838a572b2f0cb30ad467
Anwesh43 <anweshthecool0@gmail.com>
2019-05-03 11:41:55 +0530

    adding LCENode class

--- linearcircleexpandview/src/main/java/com/anwesh/uiprojects/linearcircleexpandview/LinearCircleExpandView.kt
+++ linearcircleexpandview/src/main/java/com/anwesh/uiprojects/linearcircleexpandview/LinearCircleExpandView.kt
@@ -130,4 +130,48 @@ class LinearCircleExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LCENode(var i : Int, val state : State = State()) {
+
+        private var next : LCENode? = null
+        private var prev : LCENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LCENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLCENode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LCENode {
+            var curr : LCENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
